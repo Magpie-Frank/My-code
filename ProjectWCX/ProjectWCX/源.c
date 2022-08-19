7755047b006398bb0d7be6c4738c4ec2e06170da
张浩然 <1922870858@qq.com>
2022-08-19 23:10:38 +0800

    表白

---
+++ ProjectWCX/ProjectWCX/源.c
@@ -0,0 +1,11 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>	
+
+int main(void)
+{
+	printf("You are the bone of my bones,flesh of my flesh.\n");
+	printf("I love you.\n");
+	char fu;
+		scanf("%c",&fu);
+		return 0;
+}
